0b66eadbf1ccb06b06c9596e7520f0bec3e8bf81
Kainoa Brabo <kaibrabo@gmail.com>
2021-04-29 01:55:30 -0700

    initial, created Create/Read using form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      item: {},
+      plants: []
+    };
+  }
+
+  handleChange = (e) => {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      item: {
+        ...this.state.item,
+        [name]: value
+      }
+    })
+  }
+
+  handleSubmit = (e) => {
+    this.setState(
+      {
+        plants: [
+          ...this.state.plants,
+          this.state.item
+        ]
+      }
+    );
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Blumelist</h1>
+        </header>
+
+        <section className="main">
+          <section className="form">
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                Name:
+          <input type="text" name="name" onChange={this.handleChange} />
+              </label>
+              <label>
+                Type:
+          <input type="text" name="type" onChange={this.handleChange} />
+              </label>
+              <label>
+                Date:
+          <input type="text" name="date" onChange={this.handleChange} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+
+            <p>
+              <strong>What is Blumelist? </strong>
+            </p>
+            <p>
+
+              A platform to help growers understand a flowering plant's life cycle
+            </p>
+          </section>
+
+          <section className="output_list">
+            <ol className="list">
+              {
+                this.state.plants.map((plant, key) => {
+                  return (
+                    <li className="item" id={key}>
+                      <p>name: {plant.name}</p>
+                      <p>type: {plant.type}</p>
+                      <p>date: {plant.date}</p>
+                    </li>
+                  )
+                })
+              }
+            </ol>
+          </section>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
